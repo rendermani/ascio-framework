f277f2104db03790c32732f27944dc392b22d1bc
Manuel Lautenschlager <mlautenschlager@munmlautenschlager02.fritz.box>
2019-08-14 00:21:52 +0200

    first commit

---
+++ src/lib/v2/DomainOrderRequest.php
@@ -0,0 +1,171 @@
+<?php
+
+// XSLT-WSDL-Client. Generated DB-Model class of Domain. Can be copied and overwriten with own functions.
+
+namespace ascio\v2;
+use ascio\base\DbBase;
+
+class DomainOrderRequest {
+    /**
+     * @var Domain $domain
+     */
+    private $domain;
+    public function __construct(Domain $domain)
+    {
+        $this->domain = $domain;
+    }
+    public function register(?SubmitOptions $submitOptions = null) : Order {
+        $order = new Order();
+        $order->setType(OrderType::Register_Domain);
+        $order->setDomain($this->domain);
+        return $order;
+    }
+    public function transfer(?SubmitOptions $submitOptions = null) : Order{
+        $order = new Order();
+        $order->setType(OrderType::Transfer_Domain);
+        $order->setDomain($this->domain);
+        return $order;
+    }
+    public function registrantDetailsUpdate() : Order {        
+        $domain = new Domain();
+        $this->copyNameAndHandle($domain);
+        $domain->setRegistrant($this->domain->getRegistrant());
+        $this->dataOrHandle($domain->getRegistrant());
+        $order = new Order();
+        $order->setType(OrderType::Registrant_Details_Update);
+        $order->setDomain($domain);        
+        return $order; 
+    }
+    public function ownerChange() : Order {        
+        $domain = new Domain();
+        $this->copyNameAndHandle($domain);
+        $domain->setRegistrant($this->domain->getRegistrant());
+        if($this->domain->getAdminContact()->changes()->hasChanges()) {
+            $domain->setAdminContact($this->domain->getAdminContact());
+            $this->dataOrHandle($domain->getAdminContact());           
+        }
+        $this->dataOrHandle($domain->getRegistrant());    
+        $order = new Order();
+        $order->setType(OrderType::Owner_Change);
+        $order->setDomain($domain);        
+        return $order; 
+    }
+    public function contactUpdate() : Order {        
+        $domain = new Domain();
+        $this->copyNameAndHandle($domain);
+        $domain->setAdminContact($this->domain->getAdminContact());
+        $this->dataOrHandle($domain->getAdminContact());
+        $domain->setTechContact($this->domain->getTechContact());
+        $this->dataOrHandle($domain->getTechContact());
+        $domain->setBillingContact($this->domain->getBillingContact());
+        $this->dataOrHandle($domain->getBillingContact());
+        $domain->setResellerContact($this->domain->getResellerContact());
+        $this->dataOrHandle($domain->getResellerContact());
+        $order = new Order();
+        $order->setType(OrderType::Contact_Update);     
+        $order->setDomain($domain);         
+        return $order; 
+    }
+    public function nameserverUpdate() : Order {        
+        $domain = new Domain();
+        $this->copyNameAndHandle($domain);
+        $domain->setNameServers($this->domain->getNameServers());
+        $domain->getNameServers()->db()->_id = null;
+        $domain->getNameServers()->db()->exists = false;
+        foreach($domain->getNameServers()->objects() as $key => $ns) {
+            $this->dataOrHandle($ns);
+        } 
+        if($this->domain->getTechContact()->changes()->hasChanges()) {
+            $domain->setTechContact($this->domain->getTechContact());
+            $this->dataOrHandle($domain->getTechContact()); 
+        }
+        $order = new Order();
+        $order->setType(OrderType::Nameserver_Update);
+        $order->setDomain($domain);        
+        return $order; 
+    }
+    public function domainDetailsUpdate() : ?Order {        
+        $domain = new Domain();
+        $this->copyNameAndHandle($domain);
+        $hasChanged = false;
+        // get relevant changes
+        foreach($this->domain->changes()->getChanges() as $key => $value) {            
+            if(is_object($value) || is_array($value) || in_array($key,$this->domain->getLocks()->getLockTypes())) {
+                continue;
+            }
+            $hasChanged = true;
+            $domain->_set($key,$value);
+        }
+        // has no relevant changes
+        if(!$hasChanged) return null; 
+
+        $order = new Order();
+        $order->setType(OrderType::Domain_Details_Update);
+        $order->setDomain($domain);        
+        return $order; 
+    }
+    public function delete(?SubmitOptions $submitOptions = null) : Order {        
+        $domain = new Domain();
+        $this->copyNameAndHandle($domain);
+        $order = new Order();
+        $order->setType(OrderType::Delete_Domain);
+        $order->setDomain($domain);        
+        return $order;
+    }
+    public function renew(?SubmitOptions $submitOptions = null) : Order {        
+        $domain = new Domain();
+        $this->copyNameAndHandle($domain);
+        $order = new Order();
+        $order->setType(OrderType::Renew_Domain);
+        $order->setDomain($domain);        
+        return $order;
+    }
+    public function expire(?SubmitOptions $submitOptions = null) : Order {        
+        $domain = new Domain();
+        $this->copyNameAndHandle($domain);
+        $order = new Order();
+        $order->setType(OrderType::Expire_Domain);
+        $order->setDomain($domain);        
+        return $order;
+    }
+    public function unexpire(?SubmitOptions $submitOptions = null) : Order {        
+        $domain = new Domain();
+        $this->copyNameAndHandle($domain);
+        $order = new Order();
+        $order->setType(OrderType::Unexpire_Domain);
+        $order->setDomain($domain);        
+        return $order;
+    }
+    private function copyNameAndHandle(Domain $domain) {
+        $domain->setDomainHandle($this->domain->getDomainHandle());
+        $domain->setDomainName($this->domain->getDomainName());
+    }
+    private function dataOrHandle(?DbBase $obj) {
+        if(!$obj) return; 
+        if($obj->changes()->hasChanges()) {
+            $obj->setHandle(null);
+            $obj->db()->_id = null;
+            $obj->db()->exists = false; 
+        } else {
+            $handle = $obj->getHandle();
+            if($handle) {
+                foreach($obj->properties() as $key => $value) {
+                    $obj->_set($key,null);
+                }
+                $obj->setHandle($handle);
+            }
+        }
+    }
+    public function changeLocks() : Order {                            
+        $domain = new Domain();        
+        $domain->setDeleteLock($this->domain->getDeleteLock());
+        $domain->setTransferLock($this->domain->getTransferLock());
+        $domain->setUpdateLock($this->domain->getUpdateLock());
+        $domain->getLocks()->setChangedLocks();
+        $this->copyNameAndHandle($domain);
+        $order = new Order();
+        $order->setType(OrderType::Change_Locks);
+        $order->setDomain($domain);        
+        return $order; 
+    }
+}
