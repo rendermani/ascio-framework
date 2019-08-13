516580fbf185bf8f29c032e5297437e3beb5c11d
Manuel Lautenschlager <mlautenschlager@munmlautenschlager02.fritz.box>
2019-08-13 04:20:16 +0200

    refactor

--- src/lib/DomainBlocker.php
+++ src/lib/DomainBlocker.php
@@ -4,25 +4,27 @@ namespace ascio\lib;
 use ascio\service\v2\Order;
 
 class DomainBlocker {
-    public static function block($domainName) {
+    public static function block(string $domainName) {
         global $_AscioBlockedDomains; 
         $_AscioBlockedDomains = $_AscioBlockedDomains ?: [];
         $_AscioBlockedDomains[$domainName] ="blocked";
         $_AscioBlockedDomains = array_slice($_AscioBlockedDomains,0,10000); 
         return "blocked";
     }
-    public static function unblock($domainName) {
+    public static function unblock(string $domainName) {
         global $_AscioBlockedDomains; 
         $_AscioBlockedDomains = $_AscioBlockedDomains ?: [];
-        unset($_AscioBlockedDomains[$domainName]);
+        if(array_key_exists($domainName, $_AscioBlockedDomains)) {
+            unset($_AscioBlockedDomains[$domainName]);
+        }     
         return "unblocked";
     }
-    public static function isBlocked($domainName) : bool {
+    public static function isBlocked(string $domainName) : bool {
         global $_AscioBlockedDomains; 
         $_AscioBlockedDomains = $_AscioBlockedDomains ?: [];
         return array_key_exists($domainName,$_AscioBlockedDomains);
     }
-    public static function syncFromDb($domainName)  {
+    public static function syncFromDb(string $domainName)  {
         global $_AscioBlockedDomains; 
         //Todo: Sync DB
         //for all domains
