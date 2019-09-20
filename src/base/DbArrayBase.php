f20f8917eac0ac7049b788d9ab8391fe1334c978
Manuel Lautenschlager <mlautenschlager@munmlautenschlager02.local>
2019-09-20 20:57:19 +0200

    use ArrayTrait

--- src/base/DbArrayBase.php
+++ src/base/DbArrayBase.php
@@ -2,7 +2,12 @@
 
 namespace ascio\base;
 
-class DbArrayBase extends DbBase implements ArrayInterface,\Iterator,\countable {
+use ArrayAccess;
+use Countable;
+use Iterator;
+
+class DbArrayBase extends DbBase implements ArrayInterface,Iterator,Countable,ArrayAccess  {
+    use ArrayTrait; 
     /**
      * @var Model $_db;
      */
@@ -16,86 +21,14 @@ class DbArrayBase extends DbBase implements ArrayInterface,\Iterator,\countable 
     public function __construct($parent=null)
     {        
         parent::__construct($parent);
-    }
-    public function rewind() {
-        $this->position = 0;
-    }
-    public function current() {
-        return $this->toArray()[$this->position];
-    }
-    public function key() {
-        return $this->position;
-    }
-
-    public function next() {
-        ++$this->position;
-    }
-    public function valid() {
-        if(!is_array($this->toArray())) return false;
-        return count($this->toArray()) >= $this->position + 1;
-    }
-    public function search($attributes) {
-
-    }
-    public function push($value) {
-        $this->{$this->getArrayKey()}[] = $value;
-    }
-    public function pop() {
-        return array_pop($this->toArray());
-    }
-    public function shift() {
-        return array_shift($this->toArray());
-    }
-    public function first() {
-        return $this->toArray()[0];
-    }
-    public function last() {
-        return end($this->toArray());
-    }    
-    public function index($nr) {
-        return $this->toArray()[$nr];
-    }
-    public function toArray() {
-        $value = $this->_get($this->getArrayKey());
-        return  $value ? $value : [] ;
-    }
-    public function fromArray($array) {      
-        $this->_set($this->getArrayKey(),$array);
-    }
-    public function toJson() {
-        return json_encode($this->toArray());
-    }
-    public function fromJson($json) {
-        return $this->fromArray(json_decode($json));
-    }
-    protected function add($var,$class,$args) {
-        $newObject = new $class;
-        $newObject->parent($this);
-        foreach($args as $nr => $arg) {
-            $key = $newObject->properties()->index($nr);
-            $newObject->set($key,$arg);
-        }
-        if($this->_get($var)) {
-            $this->$var[] = $newObject;            
-        } else {
-            $this->_set($var,[$newObject]);
-        }
-
-        return $newObject;
     } 
-    public function getArrayKey() {
-        return $this->_apiObjects[0];
-    }
-    public function count() : int {        
-        return count($this->toArray());
-    }
     public function createItem($data=null) : BaseClass {
-        if($data instanceof BaseClass) {
-            $arrayItem = $data; 
-        } elseif(is_object($data)) {
+        if(is_object($data)) {
             $arrayItem = $this->createProperty($this->getArrayKey());
             $arrayItem->deserialize($data);
-        }      
+        } else {
+            $arrayItem = $data;
+        };    
         return $arrayItem; 
     }
     public function create($property,$class) {
