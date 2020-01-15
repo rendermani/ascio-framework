deb62f5c64d98185d3d8e579d97215c26470b472
Manuel Lautenschlager <mlautenschlager@munmlautenschlager02.local>
2020-01-16 00:04:56 +0100

    create passwords

--- bin/init.sh
+++ bin/init.sh
@@ -4,21 +4,26 @@ root="`dirname "$0"`/../"
 cd $root
 FILE=.env
 if [ -f "$FILE" ]; then
-    echo "$FILE exist"
+    echo "$FILE exists"
 else 
-    echo "$FILE does not exist. Creating $file"
+    echo "Creating $file"
     cp .env.dist .env
 fi
 chmod u+x $root/bin/* 
 FILE=config/accounts
 if [ -f "$FILE" ]; then
-    echo "$FILE exist"
+    echo "$FILE exists"
 else 
-    echo "$FILE does not exist. Creating $file"
+    echo "Creating $file"
     cp -R config/accounts.dist config/accounts 
 fi
+# set the .env files default passwords. don't overwrite existing passwords. only replace xxxxx
+sed 's/xxxxx/'$(openssl rand -base64 29 | tr -d "=+/" | cut -c1-25)'/g' .env
+# start all docker containers
 docker-compose -f docker/docker-compose.yml up -d --remove-orphans 
+# install all php dependancies
 docker exec ascio-framework-php composer install
+# create new tables
 docker exec ascio-framework-php php database/migration/create-tables.php
-#docker exec ascio-framework-php php  -r "file_exists('.env') || copy('.env.dist', '.env');file_exists('config/accounts') || rename('config/accounts.dist', 'config/accounts');"
+# set lavarel APP_KEY
 docker exec ascio-framework-php php artisan key:generate
