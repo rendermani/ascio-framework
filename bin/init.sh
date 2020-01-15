cc61d9d6ed8ff0de3c54aa7c6afbe1177ed1efc0
Manuel Lautenschlager <mlautenschlager@munmlautenschlager02.local>
2020-01-15 17:49:20 +0100

    remove the global update

--- bin/init.sh
+++ bin/init.sh
@@ -18,7 +18,6 @@ else
     cp -R config/accounts.dist config/accounts 
 fi
 docker-compose -f docker/docker-compose.yml up -d --remove-orphans 
-docker exec ascio-framework-php composer global update
 docker exec ascio-framework-php composer install
 docker exec ascio-framework-php php database/migration/create-tables.php
 #docker exec ascio-framework-php php  -r "file_exists('.env') || copy('.env.dist', '.env');file_exists('config/accounts') || rename('config/accounts.dist', 'config/accounts');"
