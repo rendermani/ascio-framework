8eb1d88ca6309319bce2d4c6c97239cfeb1d5265
Manuel Lautenschlager <mlautenschlager@munmlautenschlager02.local>
2020-01-15 16:50:47 +0100

    check for .env

--- bin/init.sh
+++ bin/init.sh
@@ -2,8 +2,23 @@
 
 root="`dirname "$0"`/../"
 cd $root
+FILE=.env
+if [ -f "$FILE" ]; then
+    echo "$FILE exist"
+else 
+    echo "$FILE does not exist. Creating $file"
+    cp .env.dist .env
+fi
+
+FILE=config/accounts
+if [ -f "$FILE" ]; then
+    echo "$FILE exist"
+else 
+    echo "$FILE does not exist. Creating $file"
+    cp config/accounts.dist config/accounts -R 
+fi
 docker-compose -f docker/docker-compose.yml up -d --remove-orphans 
 docker exec ascio-framework-php composer install
 docker exec ascio-framework-php php database/migration/create-tables.php
-docker exec ascio-framework-php php  -r "file_exists('.env') || copy('.env.dist', '.env');file_exists('config/accounts') || rename('config/accounts.dist', 'config/accounts');"
+#docker exec ascio-framework-php php  -r "file_exists('.env') || copy('.env.dist', '.env');file_exists('config/accounts') || rename('config/accounts.dist', 'config/accounts');"
 docker exec ascio-framework-php php artisan key:generate
