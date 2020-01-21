cc7399d1459ab7356f378b941ccad99cfab7bf3b
Manuel Lautenschlager <mlautenschlager@munmlautenschlager02.local>
2020-01-21 19:35:14 +0100

    link to documentation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -80,20 +80,10 @@
             @endif
 
             <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+                <p>
+                    Please click here for the API-Documentation: <a href="https://app.swaggerhub.com/apis/rendermani/Ascio-Framework/1.0.0-oas3">SwaggerHub: REST Api documentation.</a>
+                </p>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
             </div>
         </div>
     </body>
