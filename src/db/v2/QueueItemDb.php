badc57b0519c110978694ae93d9b3b7da6434945
Manuel Lautenschlager <mlautenschlager@munmlautenschlager02.local>
2020-02-04 22:24:20 +0100

    define Msg as text

--- src/db/v2/QueueItemDb.php
+++ src/db/v2/QueueItemDb.php
@@ -5,14 +5,15 @@
 namespace ascio\db\v2;
 use ascio\base\v2\DbModel;
 
-
 class QueueItemDb extends DbModel {
 	protected $table="v2_QueueItem";
+	protected $_customColumnTypes = [
+		"Msg" => ["type" => "text"]
+	];
 	public function getAttachments(){
 		return $this->getRelationObject("v2","ArrayOfAttachment","Attachments");
 	}
 	public function getStatusList(){
 		return $this->getRelationObject("v2","ArrayOfCallbackStatus","StatusList");
 	}
-
 }
