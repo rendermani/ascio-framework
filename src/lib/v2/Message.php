d06cdcd01df82b164dc1a74e9891a1c47fd80ab4
Manuel Lautenschlager <mlautenschlager@munmlautenschlager02.local>
2020-02-05 10:18:25 +0100

    Improved Status for messages

--- src/lib/v2/Message.php
+++ src/lib/v2/Message.php
@@ -3,8 +3,16 @@
 // XSLT-WSDL-Client. Generated DB-Model class of Message. Can be copied and overwriten with own functions.
 
 namespace ascio\v2;
-use ascio\db\v2\MessageDb;
-use ascio\api\v2\MessageApi;
+use ascio\lib\StatusSerializer;
 
 class Message extends \ascio\service\v2\Message {
+    public function getStatusSerializer() : StatusSerializer
+    {      
+        parent::getStatusSerializer()->addFields([
+            "ToAddress" => $this->getToAddress(),
+            "Type" => $this->getType(), 
+            "OrderId" => $this->db()->_orderId
+        ]);
+        return $this->_statusSerializer;
+    }
 }
