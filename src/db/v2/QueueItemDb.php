79c92ded330c9ea8574db62bae18f86aa9918c12
Manuel Lautenschlager <mlautenschlager@munmlautenschlager02.local>
2020-02-18 02:49:44 +0100

    Msg column null, scope orderId

--- src/db/v2/QueueItemDb.php
+++ src/db/v2/QueueItemDb.php
@@ -7,11 +7,14 @@ use ascio\base\v2\DbModel;
 
 class QueueItemDb extends DbModel {
 	protected $table="v2_QueueItem";
-	protected $_customColumnTypes = ["Msg" => ["type" => "text"], "parameters" => ["nullable" => true]];
+	protected $_customColumnTypes = ["Msg" => ["type" => "text", "parameters" => ["nullable" => true]]];
 	public function getAttachments(){
 		return $this->getRelationObject("v2","ArrayOfAttachment","Attachments");
 	}
 	public function getStatusList(){
 		return $this->getRelationObject("v2","ArrayOfCallbackStatus","StatusList");
+	}
+	public function scopeByOrderId($query,$orderId) {
+		return $query->where("OrderId", $orderId);
 	}
 }
