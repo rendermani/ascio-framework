cc0be494a3cfa12f49e83350fa6b162e7ed568a1
Manuel Lautenschlager <mlautenschlager@munmlautenschlager02.local>
2020-03-09 16:38:28 +0100

    Custom Create Tables

--- src/db/v3/OrderInfoDb.php
+++ src/db/v3/OrderInfoDb.php
@@ -4,7 +4,7 @@
 
 namespace ascio\db\v3;
 use ascio\base\v3\DbModel;
-
+use Illuminate\Database\Schema\Blueprint;
 
 class OrderInfoDb extends DbModel {
 	protected $table="v3_OrderInfo";
@@ -15,7 +15,20 @@ class OrderInfoDb extends DbModel {
 		if(!$orderId) $orderId = $this->parent()->getOrderId();
 		assert($orderId !== null ,"OrderId exists");
 		$result = $this->where(["OrderId" => $orderId])->firstOrFail();
-		$this->parent()->set($result->attributes);	
+		$this->parent()->set($result);		
+		$this->parent()->changes()->setOriginal();
+	}
+	public function createTables(?\Closure $blueprintFunction=null) {
+		parent::createTables(function(Blueprint $table) use ($blueprintFunction){
+			$table->string('_message')->nullable();
+			$table->integer('_code')->nullable()->index();
+			$table->json('_values')->nullable();	
+			$table->string('_status')->index()->nullable();	
+			$table->string('_blocking')->index()->nullable();
+			$table->string('_topic')->index()->nullable();
+			$table->boolean('_acked')->index()->nullable();
+			if($blueprintFunction) $blueprintFunction($table);
+		}); 
 	}
 
 }
