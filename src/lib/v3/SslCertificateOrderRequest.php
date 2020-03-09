0759d2c3cf329b6b5cdf192f2cf523b18a1f96e5
Manuel Lautenschlager <mlautenschlager@munmlautenschlager02.local>
2020-03-09 16:42:15 +0100

    add getObjectName, getObjectKey

--- src/lib/v3/SslCertificateOrderRequest.php
+++ src/lib/v3/SslCertificateOrderRequest.php
@@ -4,5 +4,27 @@
 
 namespace ascio\v3;
 
-class SslCertificateOrderRequest extends \ascio\service\v3\SslCertificateOrderRequest {
+use ascio\base\OrderInterface;
+use ascio\base\TaskInterface;
+use ascio\lib\AscioOrderException;
+use ascio\lib\AscioOrderExceptionV3;
+use ascio\lib\DomainBlocker;
+use ascio\lib\OrderStatus;
+use ascio\lib\Producer;
+use ascio\lib\StatusSerializer;
+use ascio\lib\SubmitOptions;
+use ascio\lib\TaskTrait;
+
+class SslCertificateOrderRequest extends \ascio\service\v3\SslCertificateOrderRequest implements OrderInterface, TaskInterface {
+    use TaskTrait;
+    protected $orderId; 
+    protected $status; 
+    protected $objectPropertyName = "SslCertificate";
+    
+    public function getObjectName() : ?string {
+        return $this->getSslCertificate()->getCommonName();
+    }
+    public function getObjectKey() : ?string {
+        return "CommonName";
+    }      
 }
