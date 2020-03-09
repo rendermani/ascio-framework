826f571c5d9f8eb90a253a4d7c869e0ca500736e
Manuel Lautenschlager <mlautenschlager@munmlautenschlager02.local>
2020-03-09 16:41:30 +0100

    add message custom fields

--- src/db/v3/MessageDb.php
+++ src/db/v3/MessageDb.php
@@ -4,12 +4,24 @@
 
 namespace ascio\db\v3;
 use ascio\base\v3\DbModel;
-
+use Illuminate\Database\Schema\Blueprint;
 
 class MessageDb extends DbModel {
 	protected $table="v3_Message";
+	protected $_customColumnTypes = [
+		"Body" => ["type" => "text", "parameters" => ["nullable" => true]]
+	];
 	public function getAttachments(){
 		return $this->getRelationObject("v3","ArrayOfAttachment","Attachments");
+	}
+	public function createTables(?\Closure $blueprintFunction=null) {
+		parent::createTables(function(Blueprint $table) use ($blueprintFunction){
+			$table->string('_orderId')->index()->nullable();	
+			if($blueprintFunction) $blueprintFunction($table);
+		}); 
+	}
+	public function scopeOrderId($query,$orderId) {
+		return $query->where('_orderId',$orderId);
 	}
 
 }
