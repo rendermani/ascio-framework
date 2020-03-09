1d105ffd3c24cb52fe10ffff28690ccc0bb184bd
Manuel Lautenschlager <mlautenschlager@munmlautenschlager02.local>
2020-03-09 16:44:46 +0100

    fixed namespaces

--- src/lib/v3/OrderInfo.php
+++ src/lib/v3/OrderInfo.php
@@ -5,6 +5,9 @@
 namespace ascio\v3;
 
 use ascio\lib\AscioException;
+use ascio\lib\OrderStatus;
+use ascio\service\v3\OrderStatusType;
+use ReflectionClass;
 
 class OrderInfo extends \ascio\service\v3\OrderInfo {
     public function setWorkflowStatus($status=null) : OrderInfo {
@@ -15,9 +18,9 @@ class OrderInfo extends \ascio\service\v3\OrderInfo {
                 case OrderStatusType::Invalid : $this->db()->setAttribute("_status",OrderStatus::Completed);break;
             }
         } else {
-            $class = new ReflectionClass(OrderStatus::class);
+            $class = new ReflectionClass(OrderStatusType::class);
             if(!array_key_exists($status,$class->getConstants())) {
-                throw new \Exception("Status must be one of ".implode(", ",$class->getConstants()));
+                throw new \Exception("Status must be one of ".implode(", ",$class->getConstants()). " got: ". $status);
             }
             $this->db()->setAttribute("_status", $status);
         }
