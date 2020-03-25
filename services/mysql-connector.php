bb7d0ad65caf7f6172bc72996eb2709442a7e46d
Manuel Lautenschlager <mlautenschlager@munmlautenschlager02.local>
2020-03-25 10:33:56 +0100

    better errorhandling

--- services/mysql-connector.php
+++ services/mysql-connector.php
@@ -5,22 +5,29 @@ require(__DIR__."/../vendor/autoload.php");
 
 Ascio::init();
 
+use Exception;
 use Illuminate\Support\Str;
 
 Consumer::objectIncremental(function($payload) {
     Ascio::setConfig($payload->Config);
     $obj = $payload->object;    
-    if($payload->incremental) {       
-        if($payload->action=="update") {
-            $obj->getById($payload->object->db()->getKey());
-            $obj->deserialize($payload->changes);
-            $obj->db()->syncToDb();         
+    try {
+        if($payload->incremental) {       
+            if($payload->action=="update") {
+                $obj->getById($payload->object->db()->getKey());
+                $obj->deserialize($payload->changes);
+                $obj->db()->syncToDb();         
+            } else {
+                $obj->deserialize($payload->changes);
+                $obj->db()->syncToDb();
+            }
         } else {
-            $obj->deserialize($payload->changes);
-            $obj->db()->syncToDb();
-        }
-    } else {
-        $obj->db()->syncToDb(); 
-    } 
-    echo $obj->getStatusSerializer()->console(LogLevel::Info,Str::ucfirst($payload->action));  
+            $obj->db()->syncToDb(); 
+        } 
+        echo $obj->getStatusSerializer()->console(LogLevel::Info,Str::ucfirst($payload->action));  
+    } catch (Exception $e) {
+        var_dump($payload->changes);
+        $message = strpos($e->getMessage(),'Duplicate entry') === false ? $e->getMessage() : "Duplicate entry." ;
+        echo $obj->getStatusSerializer()->console(LogLevel::Error,Str::ucfirst($payload->action).": ".$message);   
+    }
 });
