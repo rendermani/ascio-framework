8485f46ba3215106381ba91f7087092161c300a9
Manuel Lautenschlager <mlautenschlager@munmlautenschlager02.local>
2020-03-25 10:33:01 +0100

    improved order interfacd

--- src/base/OrderInterface.php
+++ src/base/OrderInterface.php
@@ -7,10 +7,8 @@ use ascio\lib\SubmitOptions;
 // Todo: one interface for v2 and v3 orders. 
 // common function: create, poll, queue, getMessages, properties, db, api, config, handle ...
 interface OrderInterface {
-    public function submit(?SubmitOptions $submitOptions=null) : self;
+    public function submit(?SubmitOptions $submitOptions=null) : OrderInfoInterface;
     public function queue(?SubmitOptions $submitOptions=null) : self;
-    public function getOrderId();
-    public function getStatus() : ?string;
     public function getType();
     public function shouldQueue() : bool;
     public static function mapWorflowStatus($status);
@@ -18,11 +16,9 @@ interface OrderInterface {
     public function getWorkflowStatus();
     public function getSubmitOptions();
     public function setSubmitOptions(SubmitOptions $submitOptions);
-    public function getResult();
     public function validate();
-    public function syncOld($orderId);
-    public function syncApi() : self;
-    public function getMessages();
     public function getStatusSerializer() : StatusSerializer;
     public function db($db=null);
+    public function getObjectName() : ?string;
+    public function getObjectKey() : string;
 }
