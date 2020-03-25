9f1f858658dc9d07d49a8b4945887615d221a8e5
Manuel Lautenschlager <mlautenschlager@munmlautenschlager02.local>
2020-03-25 13:32:36 +0100

    add custom fields

--- src/db/v3/QueueMessageDb.php
+++ src/db/v3/QueueMessageDb.php
@@ -3,11 +3,22 @@
 // XSLT-WSDL-Client. Generated DB-Model class of QueueMessage. Can be copied and overwriten with own functions.
 
 namespace ascio\db\v3;
-use ascio\base\v3\DbModel;
+use ascio\base\v3\DbModel;#
 
 
 class QueueMessageDb extends DbModel {
 	protected $table="v3_QueueMessage";
+	protected $_customColumnTypes = [
+		 	"Message" => [
+				"type" => "text", 
+			    "parameters" => ["nullable" => true ]
+			 ],
+		 	"ErrorCodes" => [
+				"type" => "json", 
+			    "parameters" => ["nullable" => true ]
+			 ],
+		];
+
 	public function getAttachments(){
 		return $this->getRelationObject("v3","ArrayOfAttachment","Attachments");
 	}
