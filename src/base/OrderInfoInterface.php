ea43a7f633a57d9eb3315d8966e7aae168ea6d08
Manuel Lautenschlager <mlautenschlager@munmlautenschlager02.local>
2020-03-25 11:16:19 +0100

    new

---
+++ src/base/OrderInfoInterface.php
@@ -0,0 +1,21 @@
+<?php
+namespace ascio\base;
+
+use ascio\lib\StatusSerializer;
+use ascio\lib\SubmitOptions;
+
+// Todo: one interface for v2 and v3 orders. 
+// common function: create, poll, queue, getMessages, properties, db, api, config, handle ...
+interface OrderInfoInterface {
+    public function getOrderId();
+    public function getStatus() : ?string;
+    public static function mapWorflowStatus($status);
+    public function setWorkflowStatus($status=null);
+    public function getWorkflowStatus();
+    public function getResult();
+    public function syncOld($orderId);
+    public function syncApi() : self;
+    public function getMessages();
+    public function getStatusSerializer() : StatusSerializer;
+    public function db($db=null);
+}
