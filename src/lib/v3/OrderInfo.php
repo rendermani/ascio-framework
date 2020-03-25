130c2cdec3d33a34fe43f4eb901aaea8f1e1cc41
Manuel Lautenschlager <mlautenschlager@munmlautenschlager02.local>
2020-03-25 10:25:32 +0100

    added methods so it maches to the interface.

--- src/lib/v3/OrderInfo.php
+++ src/lib/v3/OrderInfo.php
@@ -4,27 +4,84 @@
 
 namespace ascio\v3;
 
+use ascio\base\OrderInfoInterface;
+use ascio\base\OrderInterface;
+use ascio\db\v3\MessageDb;
 use ascio\lib\AscioException;
 use ascio\lib\OrderStatus;
 use ascio\service\v3\OrderStatusType;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use ReflectionClass;
 
-class OrderInfo extends \ascio\service\v3\OrderInfo {
-    public function setWorkflowStatus($status=null) : OrderInfo {
-        if($status=null) {
-            switch($this->getStatus()) {
-                case OrderStatusType::Completed : $this->db()->setAttribute("_status",OrderStatus::Completed);break;
-                case OrderStatusType::Failed : $this->db()->setAttribute("_status",OrderStatus::Completed);break;
-                case OrderStatusType::Invalid : $this->db()->setAttribute("_status",OrderStatus::Completed);break;
-            }
+class OrderInfo extends \ascio\service\v3\OrderInfo implements OrderInfoInterface {
+    public static function mapWorflowStatus($status) {
+        switch($status) {
+            case OrderStatusType::Completed :
+            case OrderStatusType::Failed    :
+            case OrderStatusType::Invalid   : return OrderStatus::Completed;
+            case OrderStatus::Queued        : return OrderStatus::Queued;
+            case OrderStatus::Submitting    : return OrderStatus::Submitting;
+            case OrderStatus::Stored        : return OrderStatus::Stored;
+            default                         : return OrderStatus::Running;
+        }
+    }
+    public function setWorkflowStatus($status=null) : self {
+        if($status==null && AbstractOrderRequest::mapWorflowStatus($this->getStatus())) {
+            $this->db()->setAttribute("_status",AbstractOrderRequest::mapWorflowStatus($this->getStatus()));
         } else {
-            $class = new ReflectionClass(OrderStatusType::class);
+            $class = new ReflectionClass(OrderStatus::class);
             if(!array_key_exists($status,$class->getConstants())) {
-                throw new \Exception("Status must be one of ".implode(", ",$class->getConstants()). " got: ". $status);
+                throw new \Exception("Status must be one of ".implode(", ",$class->getConstants()). ". Got: ".$status);
             }
             $this->db()->setAttribute("_status", $status);
         }
         return $this; 
     }
 
+    public function getResult() {
+        return $this->lastResult;
+    }
+    /**
+     * @return Order
+     */
+    public function syncOld($orderId) {
+        $this->setOrderId($orderId);
+        try {
+            $this->db()->getByOrderId($orderId);
+    } catch(ModelNotFoundException $e) {
+            $this->syncApi();
+        } 
+        return $this;       
+    }
+    /**
+     * @return Order 
+     */
+    public function syncApi() : OrderInfoInterface {        
+        $this->api()->get();
+        //$this->getDomain()->syncApi(); 
+        if( $this->getStatus() == OrderStatusType::Failed ||
+            $this->getStatus() == OrderStatusType::Invalid        
+        ) {
+            $this->db()->_blocked = true;
+            $this->db()->save();
+        }
+        return $this;
+    } 
+    public function getWorkflowStatus() : string {
+        return $this->db()->_status;        
+    }  
+    /**
+     * Get the array of Messages
+     */ 
+    public function getMessages()
+    {
+        if(count($this->Messages) > 0) return $this->Messages;
+        $messageDb = new MessageDb();
+        $messages =  $messageDb->orderId($this->getOrderId());       
+        $this->Messages = new ArrayOfMessage();
+        foreach($messages as $message) {
+            $this->Messages->add($message);
+        }
+        return $this->Messages;
+    }     
 }
