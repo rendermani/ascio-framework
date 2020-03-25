8c2c535a6ba2260506b844b29bf9748d10ebe18a
Manuel Lautenschlager <mlautenschlager@munmlautenschlager02.local>
2020-03-25 14:12:18 +0100

    Make it more flexible

--- services/sync-orders.php
+++ services/sync-orders.php
@@ -3,11 +3,27 @@ namespace ascio\services;
 
 use ascio\lib\Sync;
 use ascio\lib\Ascio;
+use ascio\v2\SearchOrderRequest;
+use DateTime;
+use SoapFault;
 
 require(__DIR__."/../vendor/autoload.php");
-
-$sync = new Sync();
-$sync->syncOrders();
+Ascio::setConfig($argv[1]);
+$searchOrderRequest = new SearchOrderRequest();
+//$types = new ArrayOfOrderStatusType();
+//$types->add(OrderStatusType::Failed);
+//$searchOrderRequest->setOrderStatusTypes($types);
+// example 2019-01-01T00:00
+if($argv[2]) {
+    $searchOrderRequest->setFromDate(new DateTime($argv[2]));
+}
+try {
+    $sync = new Sync();
+    $sync->syncOrders($searchOrderRequest,1);
+} catch (SoapFault $e) {
+    echo $e->getMessage() ." (".$e->getCode().")";  
+    echo Ascio::getClientV2()->__getLastRequest();
+}
 
 
 
