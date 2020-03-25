65cbe838c83e95ddcb46fde713212a62e75f7a5f
Manuel Lautenschlager <mlautenschlager@munmlautenschlager02.local>
2020-03-25 10:32:38 +0100

    specify return type of getClient

--- src/base/v3/ApiModel.php
+++ src/base/v3/ApiModel.php
@@ -3,10 +3,10 @@ namespace ascio\base\v3;
 use ascio\base\Rest;
 use ascio\base\ApiModelBase;
 use ascio\lib\Ascio;
-use ascio\v3\Service;
+use ascio\service\v3\Service ;
 
 class ApiModel extends ApiModelBase implements Rest  {
-    public function getClient() {
+    public function getClient() : Service {
         return Ascio::getClient("v3");
     } 
 }
