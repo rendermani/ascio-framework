c95c29459c6916a7479fc8cd275fb81235bb565e
rendermani <ml@webrender.de>
2020-09-29 20:37:12 +0200

    remove sessionId

--- src/base/dns/RequestRootElement.php
+++ src/base/dns/RequestRootElement.php
@@ -4,9 +4,6 @@ namespace ascio\base\dns;
 class RequestRootElement extends Base {
     public function __construct()
     {
-        parent::__construct();        
-        if(isset($this->sessionId)) {
-            $this->setSessionId(Ascio::getClient("dns")->getSessionId());
-        }        
+        parent::__construct();              
     }
 }
