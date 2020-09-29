bdf64a78bf149370295c1440086d6c0c8342c5ee
rendermani <ml@webrender.de>
2020-09-29 20:14:19 +0200

    Using TaskQueue Class

--- services/order-queue.php
+++ services/order-queue.php
@@ -1,6 +1,9 @@
 <?php
 namespace ascio\lib;
 
+use ascio\base\OrderInterface;
+use ascio\base\TaskInterface;
+use ascio\logic\CallbackPayload;
 use ascio\service\v2\QueueItem;
 use ascio\v2\Order;
 use ascio\v2\OrderStatusType;
@@ -10,75 +13,5 @@ use Symfony\Component\Translation\Dumper\YamlFileDumper;
 use test\Domain;
 
 require(__DIR__."/../vendor/autoload.php");
+TaskQueue::consume();
 
-//TODO: clustering - loadbalance partitions per domain
-Consumer::callback(function($payload) use ($log) {
-    Ascio::setConfig($payload->Config); 
-    /**
-     * @var Order $order
-     */
-    $status = Order::mapWorflowStatus($payload->OrderStatus);
-    /**
-     * @var Order $newOrder
-     */
-    if(
-            $status == OrderStatus::Completed && 
-            $payload->OrderStatus !== OrderStatusType::Invalid
-    ) {
-        /**
-         * @var OrderInfoInterface $order 
-         */
-        $name = "ascio\\v3\\".str_replace($payload->ObjectType,"Type","");
-        $order = $payload->ObjectType=="DomainType" ? new Order() : new $name();  
-        try {
-            $order->db()->getByOrderId($payload->OrderId);
-        } catch (ModelNotFoundException $e) {
-            echo "OrderId: ".$payload->OrderId,$e->getMessage()."\n";
-        }
-        try {
-            /**
-             * @var QueueItem $msg
-             */     
-            $order->setStatus($payload->OrderStatus);
-            $order->setWorkflowStatus();
-            echo $order->getStatusSerializer()->console(LogLevel::Info,"Completed");
-            DomainBlocker::unblock($payload->ObjectName);
-            $newOrder = $order->db()->nextDomain($payload->ObjectName);
-            $text = " next";
-        } catch (ModelNotFoundException $e) {
-            return; 
-        }        
-    } elseif ($status == OrderStatus::Queued) {
-        $newOrder = $payload->object;
-        Ascio::setConfig($newOrder->db()->_config);
-        if(DomainBlocker::isBlocked($newOrder->getDomain()->getDomainName()) || $newOrder->db()->isBlocked() || $newOrder->shouldQueue()) {
-            // if another process running don't submit. Leave queued. Will pickup the
-            // next order from the DB
-            $newOrder->setStatus(OrderStatusType::NotSet);
-            echo $newOrder->getStatusSerializer()->console(LogLevel::Warn,"Queue blocked");
-            return; 
-        }
-    } elseif ($status == OrderStatus::Submitting) {
-        $serializer = new StatusSerializer();
-        echo $serializer->setFields($payload)->console(LogLevel::Info,"Block");
-        DomainBlocker::block($payload->ObjectName);
-        return;
-    } else {
-        echo $payload->object->getStatusSerializer()->console(LogLevel::Info,"No action for ".$payload->object->getOrderStatus());
-        return;
-    }
-    try {  
-        echo $newOrder->getStatusSerializer()->console(LogLevel::Info,"Submit".$text);
-        $newOrder->submit();
-    } catch (AscioException $e) {
-        echo $newOrder->getStatusSerializer()->console(LogLevel::Error,$e->getMessage());
-        DomainBlocker::unblock($payload->ObjectName);
-        Producer::log($newOrder,[
-            "messageType" => "error",
-            "orderId" => $newOrder->getOrderId(),
-            "message" => $e->getMessage(),
-            "code" => $e->getCode()
-        ]);
-    }
-    
-});
