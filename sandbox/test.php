8a3c000b60f905260b58fbc6f3f7fad92de798ca
rendermani <ml@webrender.de>
2020-09-29 20:18:58 +0200

    improved example

--- sandbox/test.php
+++ sandbox/test.php
@@ -1,16 +1,12 @@
 <?php
-require_once(__DIR__."/../vendor/autoload.php");
-require_once(__DIR__."/../src/lib/Rest.php");
-use ascio\lib\Abc;
-use ascio\lib\Ascio;
-use ascio\v2\Contact;
-use ascio\lib\Changes;
-use ascio\lib\Rest;
 
+use ascio\lib\Ascio;
+use ascio\v2\Order;
+
+require_once(__DIR__."/../vendor/autoload.php");
 Ascio::setConfig();
 
-var_dump(class_exists("Abc"));
 
-class ttt implements Rest {
+$order = new Order();
+$order->getById("ascio.object.5f7236bb7c1e79.06187896");
 
-}
