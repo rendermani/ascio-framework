aee0ffe75c3cd46a8928222c444f3783a5aa9161
rendermani <ml@webrender.de>
2020-09-29 21:03:40 +0200

    extends CallbackPayload instead of BlockPayload

--- src/logic/BlockPayload.php
+++ src/logic/BlockPayload.php
@@ -6,16 +6,18 @@ use ascio\base\OrderInterface;
 use ascio\lib\OrderStatus;
 use ascio\lib\TopicProducer;
 
-class BlockPayload extends Payload {
+class BlockPayload extends CallbackPayload {
     /**
      * @var OrderInterface $order 
      */
     public $workflowStatus;
 
-    public function __construct(OrderInterface $order)
+    public function __construct(OrderInterface $order = null)
     {      
        $this->class = get_class($this);
-       $this->setObjectName($order->getObjectName());
+       if ($order) {
+           $this->setObjectName($order->getObjectName());
+       }
        $this->setModule("DomainBlocker");
     }
     public function send() {
