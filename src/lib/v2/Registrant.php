1ad5ae49ba7f026b440c9ca284171f6bd310e8ab
rendermani <ml@webrender.de>
2020-09-29 20:57:48 +0200

    add validateCountryCode()

--- src/lib/v2/Registrant.php
+++ src/lib/v2/Registrant.php
@@ -5,6 +5,21 @@
 namespace ascio\v2;
 use ascio\db\v2\RegistrantDb;
 use ascio\api\v2\RegistrantApi;
+use ascio\lib\ValidationException;
 
 class Registrant extends \ascio\service\v2\Registrant {
+    function validateCountryCode($objectName=null) {
+        if(!preg_match("/[A-Z]{2}/",$this->getCountryCode())) {
+            $error = new ValidationException("Invalid country code: ".$this->getCountryCode(),412);
+            $error->setObjectName($objectName);
+            if($this->parent()) {
+                $error->setOrder($this->parent()->parent());
+            }
+            throw($error);
+        }
+        return true;
+    }
+    public function validate($objectName = null) {
+        $this->validateCountryCode($objectName);
+    }
 }
