fa9708c4108f091d2f83d95057a9608e7f0732a0
rendermani <ml@webrender.de>
2020-09-29 20:54:04 +0200

    added validateCountryCode()

--- src/lib/v2/Contact.php
+++ src/lib/v2/Contact.php
@@ -5,10 +5,26 @@
 namespace ascio\v2;
 use ascio\db\v2\ContactDb;
 use ascio\api\v2\ContactApi;
+use ascio\lib\SubmitOptions;
+use ascio\lib\ValidationException;
 
 class Contact extends \ascio\service\v2\Contact {
 
     public function update(SubmitOptions $options) {
 
+    }
+    function validateCountryCode($objectName) {
+        if(!preg_match("/[A-Z]{2}/",$this->getCountryCode())) {
+            $error = new ValidationException("Invalid country code: ".$this->getCountryCode(),412);
+            if($this->parent()) {
+                $error->setOrder($this->parent()->parent());
+            }
+            $error->setObjectName($objectName);
+            throw($error);
+        }
+        return true;
+    }
+    public function validate($objectName = null) {
+        $this->validateCountryCode($objectName);
     }
 }
