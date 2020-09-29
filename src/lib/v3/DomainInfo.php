ce927f25e356fef454d710560e95d1915bd58596
rendermani <ml@webrender.de>
2020-09-29 20:59:47 +0200

    getTld

--- src/lib/v3/DomainInfo.php
+++ src/lib/v3/DomainInfo.php
@@ -7,4 +7,7 @@ use ascio\db\v3\DomainInfoDb;
 use ascio\api\v3\DomainInfoApi;
 
 class DomainInfo extends \ascio\service\v3\DomainInfo {
+    public function getTld() : string {
+        return strtolower(implode(".",array_slice(explode(".",$this->getDomainName()),1)));
+    }
 }
