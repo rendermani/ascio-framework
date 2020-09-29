f517ff1e7f035b1f95bab85fc27de9d0a2a4d0ea
rendermani <ml@webrender.de>
2020-09-29 20:33:14 +0200

    getObjectName(), getObjectKey(), getObjectId(),..

--- src/base/TaskInterface.php
+++ src/base/TaskInterface.php
@@ -3,6 +3,7 @@ namespace ascio\base;
 
 use ascio\lib\StatusSerializer;
 use ascio\lib\SubmitOptions;
+use ascio\logic\CallbackPayload;
 
 // Todo: one interface for v2 and v3 orders. 
 // common function: create, poll, queue, getMessages, properties, db, api, config, handle ...
@@ -12,9 +13,15 @@ interface TaskInterface {
     public function getStatus() : ?string;
     public function getType() : ?string;
     public function shouldQueue() : bool;
-    public static function mapWorflowStatus($status);
+    public function getObjectName();
+    public function getObjectKey();
+    public function getObjectId();
+    public static function mapWorkflowStatus($status);
     public function setWorkflowStatus($status=null);
     public function getWorkflowStatus();
     public function getSubmitOptions();
     public function setSubmitOptions(SubmitOptions $submitOptions);
+    public function getStatusSerializer() : StatusSerializer;
+    public function db($db=null);
+    public function log($loglevel, $text, $fields = []);
 }
