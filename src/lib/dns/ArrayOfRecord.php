47a1aa7a0026f56b44bb1bf31c86a2d0e35432d6
rendermani <ml@webrender.de>
2020-09-29 20:52:45 +0200

    added clean and searchtype()

--- src/lib/dns/ArrayOfRecord.php
+++ src/lib/dns/ArrayOfRecord.php
@@ -4,5 +4,27 @@
 
 namespace ascio\dns;
 
+use ascio\lib\Ascio;
+
 class ArrayOfRecord extends \ascio\service\dns\ArrayOfRecord {
+    public function clean() : ArrayOfRecord {
+        $arrayOfRecord = new ArrayOfRecord();
+        foreach($this as $record) {
+            if (method_exists($record,"clean")) {
+                if($record->clean()) {
+                    $arrayOfRecord->add($record);                    
+                }
+            }
+        }
+        return $arrayOfRecord;
+    }
+    public function searchType(Record $recordType) : ArrayOfRecord {
+        $arrayOfRecord = new ArrayOfRecord();
+        foreach($this as $record) {
+            if(get_class($record) == get_class($recordType)) {
+                $arrayOfRecord->add($record);
+            }
+        }
+        return $arrayOfRecord;
+    }
 }
