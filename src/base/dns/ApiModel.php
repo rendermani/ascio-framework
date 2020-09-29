b0c14b206b6fb381c5a01c796b48fd9dc95a483a
rendermani <ml@webrender.de>
2020-09-29 20:34:28 +0200

    fixed namespaces

--- src/base/dns/ApiModel.php
+++ src/base/dns/ApiModel.php
@@ -1,10 +1,12 @@
-<?
+<?php
 namespace ascio\base\dns;
-use ascio\base\Rest;
 use ascio\base\ApiModelBase;
+use ascio\base\Rest;
+use ascio\lib\Ascio;
 
 class ApiModel extends ApiModelBase implements Rest  {
     public function getClient() {
         return Ascio::getClient("dns");
     } 
+
 }
