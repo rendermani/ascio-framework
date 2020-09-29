26c2139a16e92b1c3c165ad3167b9e9647c9c2e4
rendermani <ml@webrender.de>
2020-09-29 21:59:19 +0200

    move config to project config

--- bin/update-classes.php
+++ bin/update-classes.php
@@ -1,20 +1,24 @@
 <?php
 require_once(__DIR__."/../vendor/rendermani/wsdl-client-generator/src/Generator.php");
 use rendermani\wsdl\client\Generator;
+require(__DIR__."/../vendor/autoload.php");
 
-$path= realpath(__DIR__."/../src");
-echo "v2\n";
-$Generator = new Generator("Ascio v2","v2","https://aws.ascio.com/2012/01/01/AscioService.xml");
-$Generator->setCodeLang("php");
-$Generator->setOutputPath($path);
-$Generator->generate();
+$path = realpath(__DIR__."/../src");
+$configPath = realpath(__DIR__."/../config/code-generator");
+$generator = new Generator("Ascio v2","v2","https://aws.ascio.com/2012/01/01/AscioService.xml");
+$generator->setConfigPath($configPath);
+$generator->setCodeLang("php");
+$generator->setOutputPath($path);
+$generator->generate();
 echo "v3\n";
-$Generator = new Generator("Ascio v3","v3","https://aws.ascio.com/v3/aws.wsdl");
-$Generator->setCodeLang("php");
-$Generator->setOutputPath($path);
-$Generator->generate();
+$generator = new Generator("Ascio v3","v3","https://aws.ascio.com/v3/aws.wsdl");
+$generator->setConfigPath($configPath);
+$generator->setCodeLang("php");
+$generator->setOutputPath($path);
+$generator->generate();
 echo "dns\n";
-$Generator = new Generator("AscioDNS ","dns","https://dnsservice.ascio.com/2010/10/30/DnsService.wsdl");
-$Generator->setCodeLang("php");
-$Generator->setOutputPath($path);
-$Generator->generate();
+$generator = new Generator("AscioDNS ","dns","https://dnsservice.ascio.com/2010/10/30/DnsService.wsdl");
+$generator->setConfigPath($configPath);
+$generator->setCodeLang("php");
+$generator->setOutputPath($path);
+$generator->generate();
