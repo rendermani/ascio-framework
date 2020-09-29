befe87af12a4f6874fa4b1aad22eede4d229f2b2
rendermani <ml@webrender.de>
2020-09-30 00:39:01 +0200

    redundant, use init.sh

--- bin/install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker-compose -f ../docker/docker-compose.yml run  php composer install
-docker-compose  -f ../docker/docker-compose.yml  run  php php ../database/migration/create-tables.php
-docker-compose  -f ../docker/docker-compose.yml  down
