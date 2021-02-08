755bee6ad65870930b7489241e5cbda78cad6748
rendermani <ml@webrender.de>
2021-02-08 12:46:00 +0100

    new workflow v3

---
+++ sandbox/workflow-v3.php
@@ -0,0 +1,29 @@
+<?php
+namespace ascio\lib;
+use ascio\v3\TestLib;
+
+
+require(__DIR__."/../vendor/autoload.php");
+Ascio::setConfig();
+
+$domain = TestLib::getDomain("test-workflow-producequeue-".uniqid().".com");
+$domain->setDeleteLock("Lock");
+// register domain
+$domain->register((new SubmitOptions)->setQueue(true));
+// set AutoRenew
+$domain->getAutoRenew()->set(false);
+$wf = new Workflow($domain);
+// if the orders of the workflow require and unlock it will be done before submitting the orders.
+// After the orders are finished the relock will be restored if possible.
+$wf->getSubmitOptions()->setAutoUnlock(true);
+$wf->addTasks($domain->getUpdateOrders());
+/* 
+foreach($wf->getOrderRequests() as $order)  {
+    echo $order->getStatusSerializer()->console(LogLevel::Debug,"Debug Orders");
+}
+//echo $wf->debug();
+// submit workflow
+*/
+
+$wf->submit();
+
