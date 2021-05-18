635bb504122aad4e1496b3e057506b8da2a9e3ed
rendermani <ml@webrender.de>
2021-05-19 00:58:43 +0200

    improva path

--- bin/up-all.sh
+++ bin/up-all.sh
@@ -3,4 +3,4 @@ root="`dirname "$0"`/../"
 cd $root
 # start with all examples
 echo "Starting with all examples connectors (CouchDB, Redis)"
-docker-compose -f docker/docker-compose.yml up -d --remove-orphans 
+docker-compose -f $root/docker/docker-compose.yml up -d --remove-orphans 
