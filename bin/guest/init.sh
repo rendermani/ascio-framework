e458f1f398c05880be16b12234dede00d9f9d83c
rendermani <ml@webrender.de>
2021-05-19 00:58:24 +0200

    remove composer

--- bin/guest/init.sh
+++ bin/guest/init.sh
@@ -1,37 +1,31 @@
 #!/bin/bash
 
-root="`dirname "$0"`/../"
+root="`dirname "$0"`/../.. "
 cd $root
-FILE=.env
+FILE=$root/.env
 AscioPW=$(openssl rand -base64 29 | tr -d "=+/" | cut -c1-25)
 
 if [ -f "$FILE" ]; then
     echo "$FILE exists"
 else 
     echo "Creating $FILE"
-    sed 's/xxxxx/'$AscioPW'/g' .env.dist > .env
+    sed 's/xxxxx/'$AscioPW'/g' $root/.env.dist > $root/.env
 fi
 chmod u+x $root/bin/* 
-FILE=config/accounts
+FILE=$root/config/accounts
 if [ -f "$FILE/default.json" ]; then
     echo "$FILE exists"
 else 
     echo "Creating $FILE"
-    cp -R config/accounts.dist config/accounts
-    sed 's/xxxxx/'$AscioPW'/g' config/accounts.dist/default.json > config/accounts/default.json 
+    cp -R $root/config/accounts.dist $root/config/accounts
+    sed 's/xxxxx/'$AscioPW'/g' $root/config/accounts.dist/default.json > $root/config/accounts/default.json 
 fi
-FILE=bin/up-custom.sh
+FILE=$root/bin/up-custom.sh
 if [ -f "$FILE" ]; then
     echo "$FILE exists"
 else 
     echo "Creating $FILE"
-    cp bin/up.sh bin/up-custom.sh
+    cp $root/bin/up.sh $FILE
 fi
-# start all docker containers
-# install all php dependancies
-if [[ -n $1 ]]; then
-    composer config -g github-oauth.github.com $1
-fi
-composer install
-composer install --optimize-autoloader --no-dev
+
 
