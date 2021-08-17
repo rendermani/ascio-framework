93d2c79798f2f1ca6cb8f8fdf13bd13cf8d83744
rendermani <ml@webrender.de>
2021-08-17 22:50:40 +0200

    set Objectname

--- src/lib/v3/DomainInfo.php
+++ src/lib/v3/DomainInfo.php
@@ -10,4 +10,23 @@ class DomainInfo extends \ascio\service\v3\DomainInfo {
     public function getTld() : string {
         return strtolower(implode(".",array_slice(explode(".",$this->getDomainName()),1)));
     }
+    /**
+     * Get the value of objectName
+     */ 
+    public function getObjectName() : ?string
+    {
+        return $this->db()->_objectName ?: $this->getOrderRequest()->getObjectName();
+    }
+
+    /**
+     * Set the value of objectName
+     *
+     * @return  self
+     */ 
+    public function setObjectName(?string $objectName)
+    {
+        $this->db()->_objectName = $objectName;
+
+        return $this;
+    }
 }
