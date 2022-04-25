1a642e4875e7fb464dceac458c7810287a9576fd
rendermani <ml@webrender.de>
2022-04-25 13:11:37 +0200

    add blueprint

--- src/db/v3/DomainInfoDb.php
+++ src/db/v3/DomainInfoDb.php
@@ -1,10 +1,9 @@
 <?php
 
 // XSLT-WSDL-Client. Generated DB-Model class of DomainInfo. Can be copied and overwriten with own functions.
-
 namespace ascio\db\v3;
 use ascio\base\v3\DbModel;
-
+use Illuminate\Database\Schema\Blueprint;
 
 class DomainInfoDb extends DbModel {
 	protected $table="v3_DomainInfo";
